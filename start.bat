6cc998c020f679a987843fd70e4ef0ec6b3f93ba
PARKSAMJONG <cogitur@gmail.com>
2026-07-04 10:07:14 +0900

    start.bat: Anaconda 대신 Python 3.12으로 서버 기동 수정

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-start "Spectrum Dashboard" /min "C:\Users\user\AppData\Roaming\Python\Python38\Scripts\uvicorn.exe" main:app --host 0.0.0.0 --port 8000
-timeout /t 2 /nobreak >nul
+start "Spectrum Dashboard" /min "C:\Users\user\AppData\Local\Programs\Python\Python312\python.exe" -m uvicorn main:app --host 0.0.0.0 --port 8000
+timeout /t 3 /nobreak >nul
 start "" "http://localhost:8000"
